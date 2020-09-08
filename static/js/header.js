73427d44461126b48992e8fcc3e5a928e525bde1
epsxy <epsxy@users.noreply.github.com>
2020-09-09 01:47:35 +0200

    refactor: rewrite javascript and set dark theme as the default theme

--- static/js/header.js
+++ static/js/header.js
@@ -1,33 +1,33 @@
-var $sunIcon = document.getElementsByClassName('sun-btn')[0],
+const $sunIcon = document.getElementsByClassName('sun-btn')[0],
     $moonIcon = document.getElementsByClassName('moon-btn')[0];
 
-if (localStorage.getItem('theme') && localStorage.getItem('theme') === 'dark') {
+const applyDarkTheme = () => {
+    localStorage.setItem('theme', 'dark');
     document.body.classList.add('dark');
+    document.body.classList.remove('light');
     removeClass($sunIcon, 'hidden');
     addClass($moonIcon, 'hidden');
+};
+
+const applyLightTheme = () => {
+    localStorage.setItem('theme', 'light');
+    document.body.classList.add('light');
+    document.body.classList.remove('dark');
+    addClass($sunIcon, 'hidden');
+    removeClass($moonIcon, 'hidden');
+};
+
+if (localStorage.getItem('theme') === 'dark') {
+    applyDarkTheme();
+} else if (localStorage.getItem('theme') === 'light') {
+    applyLightTheme();
+} else {
+    /* default theme is dark theme */
+    applyDarkTheme();
 }
 
-$sunIcon.addEventListener(
-    'click',
-    function() {
-        localStorage.setItem('theme', 'light');
-        document.body.classList.remove('dark');
-        addClass($sunIcon, 'hidden');
-        removeClass($moonIcon, 'hidden');
-    },
-    false,
-);
-
-$moonIcon.addEventListener(
-    'click',
-    function() {
-        localStorage.setItem('theme', 'dark');
-        document.body.classList.add('dark');
-        removeClass($sunIcon, 'hidden');
-        addClass($moonIcon, 'hidden');
-    },
-    false,
-);
+$sunIcon.addEventListener('click', applyLightTheme, false);
+$moonIcon.addEventListener('click', applyDarkTheme, false);
 
 function addClass(element, className) {
     element.className += ' ' + className;
@@ -36,6 +36,6 @@ function addClass(element, className) {
 function removeClass(element, className) {
     // Capture any surrounding space characters to prevent repeated
     // additions and removals from leaving lots of spaces.
-    var classNameRegEx = new RegExp('\\s*' + className + '\\s*');
+    const classNameRegEx = new RegExp('\\s*' + className + '\\s*');
     element.className = element.className.replace(classNameRegEx, ' ');
 }
